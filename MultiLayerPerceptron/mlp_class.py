7fda4e77cf735bebd30b3f34826e7992d4f1f5c1
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2018-11-24 15:51:20 +0100

    Add checkpoint folder to gitignore. Update mlp class with summaries and checkpoint save. Add global step. Update tensorboard visualization

--- MultiLayerPerceptron/mlp_class.py
+++ MultiLayerPerceptron/mlp_class.py
@@ -4,38 +4,46 @@ from tensorflow import Tensor
 import numpy as np
 import functools
 
-def define_scope(function):
-    attribute = '_cache_' + function.__name__
+def define_scope(name_scope=False):
+    def _(function):
+        attribute = '_cache_' + function.__name__
 
-    @property
-    @functools.wraps(function)
-    def decorator(self):
-        if not hasattr(self, attribute):
-            with tf.variable_scope(function.__name__):
-                setattr(self, attribute, function(self))
-        return getattr(self, attribute)
+        @property
+        @functools.wraps(function)
+        def decorator(self):
+            if not hasattr(self, attribute):
+                if(name_scope):
+                    with tf.name_scope(function.__name__):
+                        setattr(self, attribute, function(self))
+                else:
+                    with tf.variable_scope(function.__name__):
+                        setattr(self, attribute, function(self))
+            return getattr(self, attribute)
 
-    return decorator
+        return decorator
+    return _
+
 
 def perceptron_layer(x: Tensor, out_dim, batch_norm=False, activation_fn=tf.sigmoid, name="hidden_layer"):
     in_dim = x.get_shape().as_list()[1]
-    with tf.variable_scope(name):
+    with tf.variable_scope(name) as v_scope:
         w = tf.Variable(initial_value=tf.random_normal((in_dim, out_dim)))
         b = tf.Variable(initial_value=tf.random_normal((out_dim,)))
         prod = tf.einsum('ij,jk->ik', x, w)
         if(batch_norm):
-            with tf.variable_scope('batch_norm'):
-                mean, var = tf.nn.moments(prod, axes=0)
-                v = tf.Variable(tf.ones([out_dim]))
-                prod = tf.div_no_nan(prod - mean, tf.sqrt(var + 1e-3))
-                prod = v * prod + b
+            prod, b_scope = batch_normalization(prod, out_dim, b)
         else:
             prod = prod + b
 
+        total_v_coll = v_scope.global_variables()
+        if(b_scope):
+            total_v_coll.append(b_scope.global_variables())
+
         if(activation_fn != None):
-            return activation_fn(prod)
+            return activation_fn(prod), total_v_coll
         else:
-            return prod
+            return prod, total_v_coll
+
 
 def apply_dropout(x, keep_prob):
     def _():
@@ -43,31 +51,49 @@ def apply_dropout(x, keep_prob):
         return tf.where(tf.less_equal(p, keep_prob), x, tf.zeros_like(x))
     return _
 
+
 def dropout(x: Tensor, is_training, keep_prob=0.5):
     with tf.variable_scope("dropout"):
         return tf.cond(tf.equal(is_training, True), true_fn=apply_dropout(x, keep_prob), false_fn=lambda: x)
 
-def batch_normalization(x: Tensor):
-    mean = tf
-    pass
+
+def batch_normalization(x: Tensor, out_dim, bias):
+    with tf.variable_scope('batch_norm') as scope:
+        mean, var = tf.nn.moments(x, axes=0)
+        v = tf.Variable(tf.ones([out_dim]))
+        x = tf.div_no_nan(x - mean, tf.sqrt(var + 1e-3))
+        return v * x + bias, scope
 
 
 class MultiLayerPerceptron:
-    def __init__(self, layer_structure: list, data: Tensor, targets: Tensor, is_training: Tensor, learning_rate=0.01, use_tf_model=True):
+    def __init__(self, layer_structure: list, data: Tensor, targets: Tensor, is_training: Tensor, learning_rate=0.01, use_tf_model=True, step=None):
         if(use_tf_model):
-            self.model = self.inference_tf(data, is_training, layer_structure)
+            self.model, self._v_coll = self.inference_tf(data, is_training, layer_structure)
         else:
-            self.model = self.inference(data, is_training, layer_structure)
+            self.model, self._v_coll = self.inference(data, is_training, layer_structure)
 
+        self.step = step
         self.data = data
         self.targets = targets
         self.learning_rate = learning_rate
         self.accuracy
         self.optimize
         self.loss
+        self.summary
+
+    @define_scope(True)
+    def summary(self):
+        with tf.name_scope("summaries"):
+            tf.summary.scalar("loss", self.loss)
+            tf.summary.scalar("accuracy", self.accuracy)
+            tf.summary.histogram("histogram loss", self.loss)
+            return tf.summary.merge_all()
+
+    def get_vars_to_save(self):
+        return self._v_coll
 
     def inference_tf(self, x, is_training, layer_structure):
-        with tf.variable_scope('network'):
+        with tf.variable_scope('network') as n_scope:
             data = x
 
             for l_index in range(0, len(layer_structure) - 1):
@@ -84,38 +110,47 @@ class MultiLayerPerceptron:
                 data, layer_structure[-1], activation_fn=None)
             y = tf.layers.batch_normalization(y, training=is_training)
 
-            return tf.nn.softmax(y)
+            total_v_coll = n_scope.global_variables()
+
+            return tf.nn.softmax(y), total_v_coll
 
     def inference(self, x, is_training, layer_structure):
-        with tf.variable_scope('network'):
+        with tf.variable_scope('network') as n_scope:
             data = x
+
+            total_v_coll = n_scope.global_variables()
 
             for l_index in range(0, len(layer_structure) - 1):
                 l_structure = layer_structure[l_index]
-                data = perceptron_layer(
+                data, l_scope = perceptron_layer(
                     data, l_structure, batch_norm=True)
                 data = dropout(data, is_training, keep_prob=0.4)
+                if(l_scope):
+                    total_v_coll.append(l_scope)
 
-            y = perceptron_layer(
+            y, l_scope = perceptron_layer(
                 data, layer_structure[-1], batch_norm=True, activation_fn=tf.nn.softmax, name="out_layer")
 
-            return y
+            if(l_scope):
+                total_v_coll.append(l_scope)
 
-    @define_scope
+            return y, total_v_coll
+
+    @define_scope()
     def accuracy(self):
         pred_index = tf.argmax(self.model, axis=1)
         targ_index = tf.argmax(self.targets, axis=1)
         return tf.reduce_mean(
             tf.cast(tf.equal(pred_index, targ_index), dtype=tf.float32))
 
-    @define_scope
+    @define_scope()
     def loss(self):
         return -tf.reduce_sum(tf.reduce_mean(self.targets * tf.log(self.model), axis=1))
 
-    @define_scope
+    @define_scope()
     def optimize(self):
         # UPDATE_OPS necessary to use batch normalization
         with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
-            # Define train step using the AdamOptimizer
+            # Define train step using the AdamOptimizer with global_step increment
             return tf.train.AdamOptimizer(
-                learning_rate=self.learning_rate).minimize(self.loss)
+                learning_rate=self.learning_rate).minimize(self.loss, global_step=self.step)
--- MultiLayerPerceptron/mlp_class_runner.py
+++ MultiLayerPerceptron/mlp_class_runner.py
@@ -4,9 +4,12 @@ from matplotlib import pyplot as plt
 from tensorflow.python import debug as tf_debug
 import numpy as np
 from mlp_class import MultiLayerPerceptron
+import os
+from datetime import datetime
+now = datetime.now()
+logdir = "graphs/" + now.strftime("%Y%m%d-%H%M%S") + "/"
 
-
-def main():
+def main(restore_save=False):
     """
     Placeholders for input 
     NB images are expressed in terms of vector and not matrices. 
@@ -19,14 +22,27 @@ def main():
     # Placeholder for discerning train/eval mode
     is_training = tf.placeholder(dtype=tf.bool)
 
+    # Define global step to indicize checkpoint saves
+    global_step = tf.Variable(0, dtype=tf.int32, trainable=False, name="global_step")
+
     mnist = input_data.read_data_sets('/tmp/mnist', one_hot=True)
 
-    model = MultiLayerPerceptron([256, 64, 10], x, targets, is_training)
+    model = MultiLayerPerceptron([256, 64, 10], x, targets, is_training, step=global_step, learning_rate=0.0000975)
     init_op = tf.global_variables_initializer()
 
-    writer = tf.summary.FileWriter('./graphs', tf.get_default_graph())
+    writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+    saver = tf.train.Saver(model.get_vars_to_save())
     with tf.Session() as sess:
-        writer = tf.summary.FileWriter('./graphs', sess.graph)
+        writer = tf.summary.FileWriter(logdir, sess.graph)
+
+        if(restore_save):
+           # check if there is checkpoint
+            ckpt = tf.train.get_checkpoint_state(os.path.dirname('checkpoints/checkpoint'))
+
+            # check if there is a valid checkpoint path
+            if ckpt and ckpt.model_checkpoint_path:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                print("Checkpoint restored: {}".format(ckpt.model_checkpoint_path))
 
         #sess = tf_debug.LocalCLIDebugWrapperSession(sess)
 
@@ -34,7 +50,7 @@ def main():
         sess.run(init_op)
 
         # Training parameters
-        training_epochs = 15
+        training_epochs = 50
         batch_size = 128
 
         # Number of batches to process to see whole dataset
@@ -47,18 +63,23 @@ def main():
                                     feed_dict={x: mnist.validation.images,
                                                targets: mnist.validation.labels,
                                                is_training: False})
+            
+            saver.save(sess, 
+                'checkpoints/mlp.ckpt',
+                global_step=global_step)
 
             print(
                 'Epoch: {:06d} - VAL accuracy: {:.03f} %'.format(epoch, val_accuracy * 100))
 
-            for _ in range(batches_each_epoch):
+            for index in range(batches_each_epoch):
 
                 # Load a batch of training data
                 x_batch, target_batch = mnist.train.next_batch(batch_size)
 
                 # Actually run one training step here
-                sess.run(fetches=[model.optimize],
+                _, summary = sess.run(fetches=[model.optimize, model.summary],
                          feed_dict={x: x_batch, targets: target_batch, is_training: True})
+                writer.add_summary(summary, global_step=index * (epoch + 1))
 
         test_accuracy = sess.run(fetches=model.accuracy,
                                  feed_dict={x: mnist.test.images,
