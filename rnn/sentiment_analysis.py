1f81d51a2698ec3af51c3037bc37e3599c96660a
lorib <lorenzobonicelli@libero.it>
2018-12-05 14:19:24 +0100

    Add dockerfiles. Add rnn exercise

---
+++ rnn/sentiment_analysis.py
@@ -0,0 +1,371 @@
+#%% [markdown]
+# Install proper packages
+
+#%%
+get_ipython().system('pip install googledrivedownloader')
+
+#%% [markdown]
+# Import proper packages and download tweets data
+
+#%%
+import re
+import csv
+import numpy as np
+import tensorflow as tf
+from collections import Counter
+from google_drive_downloader import GoogleDriveDownloader
+
+
+GoogleDriveDownloader.download_file_from_google_drive(file_id='1fHezNVY4YWJVWYb_3P3kx2e9RstjY1OK',
+                                                      dest_path='data/tweets.zip',
+                                                      unzip=True)
+
+#%% [markdown]
+# Global parameters, nothing fancy for now
+
+#%%
+EPS = np.finfo('float32').eps       # machine precision for float32
+MAX_TWEET_CHARS = 140               # each tweet is made by max. 140 characters
+
+#%% [markdown]
+# Data loading primitives, useful to load tweets and their annotation in a numerical representation
+
+#%%
+def preprocess(line):
+    """
+    Pre-process a string of text. Eventually add additional pre-processing here.
+    """
+    line = line.lower()               # turn to lowercase
+    line = line.replace('\n', '')     # remove newlines
+    line = re.sub(r'\W+', ' ', line)  # keep characters only (\W is short for [^\w])
+
+    return line
+
+
+def get_dictionary(filename, dict_size=2000):
+    """
+    Read the tweets and return a list of the 'max_words' most common words.
+    """
+    all_words = []
+    with open(filename, 'r') as csv_file:
+        r = csv.reader(csv_file, delimiter=',', quotechar='"')
+        for row in r:
+            tweet = row[3]
+            if len(tweet) <= MAX_TWEET_CHARS:
+                words = preprocess(tweet).split()
+                all_words += words
+
+    # Make the dictionary out of only the N most common words
+    word_counter = Counter(all_words)
+    dictionary, _ = zip(*word_counter.most_common(min(dict_size, len(word_counter))))
+
+    return dictionary
+
+
+class TweetLoader(object):
+
+    def __init__(self, filename_train, filename_val, batchsize, max_len, dict_size):
+
+        self._filename_train = filename_train
+        self._filename_val = filename_val
+        self._batchsize = batchsize
+        self._max_len = max_len
+        self._dict_size = dict_size
+
+        # get the list of words that will constitute our dictionary (once only)
+        self._dictionary = get_dictionary(self._filename_train, dict_size)
+
+        self._train_rows = self.read_data(self._filename_train)
+        self._val_rows = self.read_data(self._filename_val)
+
+    def read_data(self, filename):
+        # read training data
+        rows = []
+        with open(filename, 'r') as csv_file:
+            reader = csv.reader(csv_file, delimiter=',', quotechar='"')
+            for row in reader:
+                rows.append(row)
+        return rows
+
+    def vectorize(self, tweet):
+        words = preprocess(tweet).split()
+
+        X = np.zeros(shape=(1, self._max_len, self._dict_size + 1))
+
+        # Vectorization
+        for j, w in enumerate(words):
+            if j < self._max_len:
+                try:
+                    w_idx = self._dictionary.index(w)
+                    X[0, j, w_idx + 1] = 1
+                except ValueError:
+                    # Word not found, using the unknown
+                    X[0, j, 0] = 1
+
+        return X
+
+    def load_tweet_batch(self, mode):
+        """
+        Generate a batch of training data
+        """
+        assert mode in ['train', 'val']
+        if mode == 'train':
+            rows = self._train_rows
+        else:
+            rows = self._val_rows
+
+        # prepare data structures
+        X_batch = np.zeros((self._batchsize, self._max_len, len(self._dictionary) + 1), dtype=np.float32)
+        Y_batch = np.zeros((self._batchsize, 2), dtype=np.float32)
+
+        tweet_loaded = 0
+        while tweet_loaded < self._batchsize:
+
+            rand_idx = np.random.randint(0, len(rows))
+            Y_batch[tweet_loaded, int(rows[rand_idx][1])] = 1
+
+            random_tweet = rows[rand_idx][3]
+            if len(random_tweet) <= MAX_TWEET_CHARS:
+
+                X = self.vectorize(tweet=random_tweet)
+                X_batch[tweet_loaded] = X[0]
+                tweet_loaded += 1
+
+        return X_batch, Y_batch
+
+#%% [markdown]
+# Define our recurrent model for sentiment analysis on tweets: the functions you have to implement are make_inference, make_loss, make_train_step.
+
+#%%
+def length(sequence):
+    """
+    Returns the useful lenght of the sequence.
+    """
+    used = tf.sign(tf.reduce_max(tf.abs(sequence), 2))
+    length = tf.reduce_sum(used, 1)
+    length = tf.cast(length, tf.int32)
+    return length
+
+
+def last_relevant(output, length):
+    """
+    Returns the indexes of the last relevant element of the sequence.
+    """
+    batch_size = tf.shape(output)[0]
+    max_length = tf.shape(output)[1]
+    out_size = int(output.get_shape()[2])
+    index = tf.range(0, batch_size) * max_length + (length - 1)
+    flat = tf.reshape(output, [-1, out_size])
+    relevant = tf.gather(flat, index)
+    return relevant
+
+
+class TweetModel(object):
+
+    def __init__(self, x, targets, hidden_size):
+
+        self.x = x
+        self.targets = targets
+        self.n_classes = targets.get_shape()[-1]
+
+        self.hidden_size = hidden_size
+
+        self.inference = None
+        self.loss = None
+        self.train_step = None
+        self.accuracy = None
+
+        self.make_inference()
+        self.make_loss()
+        self.make_train_step()
+        self.make_accuracy()
+
+    def make_inference(self):
+
+        # Define the final prediction applying a fully connected layer with softmax
+        self.inference = None
+
+    def make_loss(self):
+        self.loss = None
+
+    def make_train_step(self):
+        self.train_step = None
+        
+    def make_accuracy(self):
+        self.accuracy = None
+
+#%% [markdown]
+# #My solution
+
+#%%
+def length(sequence):
+    """
+    Returns the useful lenght of the sequence.
+    """
+    used = tf.sign(tf.reduce_max(tf.abs(sequence), 2))
+    length = tf.reduce_sum(used, 1)
+    length = tf.cast(length, tf.int32)
+    return length
+
+
+def last_relevant(output, length):
+    """
+    Returns the indexes of the last relevant element of the sequence.
+    """
+    batch_size = tf.shape(output)[0]
+    max_length = tf.shape(output)[1]
+    out_size = int(output.get_shape()[2])
+    index = tf.range(0, batch_size) * max_length + (length - 1)
+    flat = tf.reshape(output, [-1, out_size])
+    relevant = tf.gather(flat, index)
+    return relevant
+
+
+class TweetModel(object):
+
+    def __init__(self, x, targets, hidden_size):
+
+        self.x = x
+        self.targets = targets
+        self.n_classes = targets.get_shape()[-1]
+
+        self.hidden_size = hidden_size
+
+        self.inference = None
+        self.loss = None
+        self.train_step = None
+        self.accuracy = None
+
+        self.make_inference()
+        self.make_loss()
+        self.make_train_step()
+        self.make_accuracy()
+
+    def make_inference(self):
+
+        # Create LSTM cell with proper hidden size
+        cell = tf.contrib.rnn.LSTMCell(self.hidden_size)
+
+        # Get LSTM output
+        val, state = tf.nn.dynamic_rnn(cell, self.x, sequence_length=length(self.x), dtype=tf.float32)
+
+        # Get last output of LSTM
+        last = last_relevant(val, length(val))
+
+        # Define the final prediction applying a fully connected layer with softmax
+        self.inference = tf.layers.dense(state[0], self.n_classes, activation=tf.nn.softmax)
+
+    def make_loss(self):
+        self.loss = - tf.reduce_sum(self.targets * tf.log(self.inference + EPS))
+
+    def make_train_step(self):
+        self.train_step = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
+
+    def make_accuracy(self):
+        mistakes = tf.equal(tf.argmax(self.inference, axis=1), tf.argmax(self.targets, axis=1))
+        self.accuracy = tf.reduce_mean(tf.cast(mistakes, tf.float32))
+
+#%% [markdown]
+# #Training and Test
+#%% [markdown]
+# Define some hyperparameters
+
+#%%
+# Model parameters
+max_seq_len = 20  # Maximum length (in words) of tweets
+max_dict_size = 1000  # Maximum dictionary size
+hidden_size = 10  # LSTM cell dimension
+train_tweets_path = 'data/tweets_train.csv'
+val_tweets_path = 'data/tweets_val.csv'
+
+# Training parameters
+training_epochs = 20 # Number of training epochs
+batch_size = 32 # Size of each training batch
+batches_each_epoch = 500 # Number of batches for each epochs
+
+#%% [markdown]
+# Get the TweetLoader that will provide us training batches
+
+#%%
+# Get tweet loader
+loader = TweetLoader(train_tweets_path, val_tweets_path, batch_size, max_seq_len, max_dict_size)
+x_batch, y_batch = loader.load_tweet_batch(mode='train')
+
+#%% [markdown]
+# Define placeholders for tweets (x) and sentiments (target). Be extra-careful to shapes!
+
+#%%
+# Declare placeholders
+x = tf.placeholder(shape=(None, max_seq_len, max_dict_size+1), dtype=tf.float32)
+targets = tf.placeholder(shape=(None, 2), dtype=tf.float32)
+
+#%% [markdown]
+# Instantiate the model.
+
+#%%
+# Get a model
+model = TweetModel(x, targets, hidden_size)
+
+#%% [markdown]
+# Open tensorflow session...
+
+#%%
+# Open new session
+sess = tf.Session()
+
+#%% [markdown]
+# ... and initialize variables.
+
+#%%
+# Initialize all variables
+sess.run(tf.global_variables_initializer())
+
+#%% [markdown]
+# Training loop, fill in blanks following comments
+
+#%%
+for epoch in range(training_epochs):
+
+    x_batch, y_batch = loader.load_tweet_batch(mode='train')
+    print('Epoch: {}\tTRAIN: Loss: {:.02f} Accuracy: {:.02f}'.format(
+        epoch,
+        # Compute batch loss
+        sess.run(model.loss, feed_dict={x: x_batch, targets: y_batch}),
+        # Compute batch accuracy
+        sess.run(model.accuracy, feed_dict={x: x_batch, targets: y_batch})
+    ))
+
+    x_batch, y_batch = loader.load_tweet_batch(mode='val')
+    print('Epoch: {}\tVAL: Loss: {:.02f} Accuracy: {:.02f}'.format(
+        epoch,
+        # Compute batch loss
+        sess.run(model.loss, feed_dict={x: x_batch, targets: y_batch}),
+        # Compute batch accuracy
+        sess.run(model.accuracy, feed_dict={x: x_batch, targets: y_batch})
+    ))
+
+    for _ in range(batches_each_epoch):
+
+        # Load a batch of training data
+        x_batch, y_batch = loader.load_tweet_batch(mode='train')
+
+        # Actually run one training step here
+        sess.run(fetches=[model.train_step], feed_dict={x: x_batch, targets: y_batch})
+
+
+#%%
+# Interactive session
+while True:
+    tw = raw_input('Try tweeting something!')
+    if tw:
+        x_num = loader.vectorize(tweet=tw)
+        p, = sess.run([model.inference], feed_dict={x: x_num})
+        if np.argmax(p) == 0:
+            # Negative tweet
+            print('Prediction:{}\t:('.format(p))
+        else:
+            print('Prediction:{}\t:)'.format(p))
+    else:
+        break
+
+
---
+++ rnn/sequence_counter.py
@@ -0,0 +1,244 @@
+#%% [markdown]
+# Import proper packages
+
+#%%
+import pickle
+import argparse
+import numpy as np
+import tensorflow as tf
+from os import makedirs
+from os.path import exists
+from os.path import dirname
+from random import shuffle
+
+epsilon = np.finfo(float).eps
+
+#%% [markdown]
+# This class models the dataset used for today's lecture: each example is composed of binary sequences, and the target variable encodes how many "1" there are within each sequence.
+
+#%%
+class SyntheticSequenceDataset:
+
+    def __init__(self, max_sequence_length=10, dataset_cache='/tmp/synthetic_dataset.pickle', force_recompute=True):
+
+        self._data = None
+        self.max_sequence_length = max_sequence_length
+        self.force_recompute = force_recompute
+        self.dataset_cache = dataset_cache
+
+    @property
+    def data(self):
+
+        if not self._data:
+            if not self.force_recompute and exists(self.dataset_cache):
+                print('Loading dataset from cache...')
+                with open(self.dataset_cache, 'rb') as dump_file:
+                    dataset = pickle.load(dump_file)
+            else:
+                print('Recomputing dataset...')
+                dataset = self._compute_dataset()
+                if not exists(dirname(self.dataset_cache)):
+                    makedirs(dirname(self.dataset_cache))
+                with open(self.dataset_cache, 'wb') as dump_file:
+                    pickle.dump(dataset, dump_file)
+
+            # Store data
+            self._data = dataset
+
+        return self._data
+
+    def _compute_dataset(self,):
+
+        n = self.max_sequence_length
+        num_examples = 2 ** n
+        num_classes = n + 1
+
+        # How many examples to use for training (others are for test)
+        num_train_examples = int(0.8 * num_examples)
+
+        # Generate 2**20 binary strings
+        data_strings = [('{' + '0:0{}b'.format(n) + '}').format(i) for i in range(num_examples)]
+
+        # Shuffle sequences
+        shuffle(data_strings)
+
+        # Cast to numeric each generated binary string
+        data_x, data_y = [], []
+        for i in range(num_examples):
+            train_sequence = []
+            for binary_char in data_strings[i]:
+                value = int(binary_char)
+                train_sequence.append([value])
+            data_x.append(train_sequence)           # examples are binary sequences of int {0, 1}
+            data_y.append(np.sum(train_sequence))   # targets are the number of ones in the sequence
+
+        # Convert from categorical to one-hot
+        data_y_one_hot = np.eye(num_classes)[data_y]
+
+        # Separate suggested training and test data
+        train_data      = data_x[:num_train_examples]
+        train_targets   = data_y_one_hot[:num_train_examples]
+        test_data       = data_x[num_train_examples:]
+        test_targets    = data_y_one_hot[num_train_examples:]
+
+        return train_data, train_targets, test_data, test_targets
+
+#%% [markdown]
+# Create and load the sequence dataset
+
+#%%
+hidden_size = 40
+batch_size = 128
+n_epochs = 64
+max_sequence_length = 10
+
+# Load dataset
+synthetic_dataset = SyntheticSequenceDataset(max_sequence_length=max_sequence_length)
+train_data, train_targets, test_data, test_targets = synthetic_dataset.data
+
+#%% [markdown]
+# This class represents our recurrent module for learning to count. Your job is to fill in all the methods building the graph for inference, loss function, train step.
+# You may find [tf.contrib.rnn.LSTMCell](https://www.tensorflow.org/api_docs/python/tf/contrib/rnn/LSTMCell), [tf.nn.dynamic_rnn](https://www.tensorflow.org/api_docs/python/tf/nn/dynamic_rnn), [tf.layers.dense](https://www.tensorflow.org/api_docs/python/tf/layers/dense) useful
+
+#%%
+# Define placeholders
+data = None # Define the placeholder for the input data sequences
+targets = None # Define the placeholder for the ground truth
+
+class DeepCounter:
+
+    def __init__(self, x, targets, hidden_size):
+
+        self.x = x
+        self.targets = targets
+        self.n_classes = targets.get_shape()[-1]
+
+        self.hidden_size = hidden_size
+
+        self.inference = None
+        self.loss = None
+        self.train_step = None
+        self.accuracy = None
+
+        self.make_inference()
+        self.make_loss()
+        self.make_train_step()
+        self.make_accuracy()
+
+    def make_inference(self):
+        if self.inference is None:
+            """
+            TODO:
+            1) Create a recurrent cell (RNN, LSTM, GRU etc..)
+            2) Create a recurrent neural network specified by the cell, thus 
+            performing a fully dynamic unrolling of the inputs (stored in self.x)
+            3) Take the last output of the sequence
+            4) Append a classification layer.
+            """"
+            self.inference = None
+
+    def make_loss(self):
+        if self.loss is None:
+            self.loss = # Build cross-entropy loss
+
+    def make_train_step(self):
+         and training step
+        if self.train_step is None:
+            optimizer = # Define Optimizer
+            self.train_step = # Define the training step
+
+    def make_accuracy(self):
+        if self.accuracy is None:
+            correct_predictions = None # Define the accuracy tensor
+
+deep_counter = DeepCounter(x=data, targets=targets, hidden_size=hidden_size)
+
+#%% [markdown]
+# #Training and Test
+# 
+# 
+# 
+# 
+#%% [markdown]
+# Training loop. Your role is to fill the code to run one optimization step
+
+#%%
+# Open session
+sess = tf.Session()
+
+# Initialize variables
+sess.run(tf.global_variables_initializer())
+
+batches_each_epoch = int(len(train_data)) // batch_size
+
+print('\n' + 50*'*' + '\nTraining\n' + 50*'*')
+
+# Train batch by batch
+for epoch in range(0, n_epochs):
+
+    start_idx = 0
+    loss_current_epoch = []
+    for _ in range(0, batches_each_epoch):
+
+        # Load batch
+        end_idx = start_idx + batch_size
+        data_batch, target_batch = train_data[start_idx:end_idx], train_targets[start_idx:end_idx]
+
+        # Run one optimization step on current step
+        cur_loss, _ = sess.run([deep_counter.loss, deep_counter.train_step], feed_dict={data: data_batch, targets: target_batch})
+        loss_current_epoch.append(cur_loss)
+
+        # Update data pointer
+        start_idx += batch_size
+    
+    if epoch % 10 == 0:
+      print('Epoch {:02d} - Loss on train set: {:.02f}'.format(epoch, sum(loss_current_epoch)/batches_each_epoch))
+
+#%% [markdown]
+# Run test. Your role is to fill code to run the graph and compute accuracy
+
+#%%
+print('\n' + 50 * '*' + '\nTesting\n' + 50 * '*')
+
+accuracy_score = 0.0
+num_test_batches = int(len(test_data)) // batch_size
+start_idx = 0
+
+# Test batch by batch
+for _ in range(0, num_test_batches):
+    end_idx = start_idx + batch_size
+    data_batch, target_batch = test_data[start_idx:end_idx], test_targets[start_idx:end_idx]
+    # compute accuracy on batch
+    accuracy_score += sess.run(deep_counter.accuracy, feed_dict={data: data_batch, targets: target_batch})
+    start_idx += batch_size
+
+print('Average accuracy on test set: {:.03f}'.format(accuracy_score / num_test_batches))
+
+#%% [markdown]
+# Interactive section, just try to stress the model typing sequences yourself! :)
+
+#%%
+print('\n' + 50 * '*' + '\nInteractive Session\n' + 50 * '*')
+
+while True:
+    my_sequence = raw_input('Write your own binary sequence 10 digits in {0, 1}:\n')
+    if my_sequence:
+
+        # Pad shorter sequences
+        if len(my_sequence) < max_sequence_length:
+            my_sequence = (max_sequence_length - len(my_sequence))*'0' + my_sequence
+
+        # Crop longer sequences
+        my_sequence = my_sequence[:max_sequence_length]
+
+        # Prepare example
+        test_example = []
+        for binary_char in my_sequence:
+            test_example.append([float(binary_char)])
+
+        pred = sess.run(deep_counter.inference, feed_dict={data: np.expand_dims(test_example, 0)})
+        print('Predicted number of ones: {} - Real: {}\n'.format(int(np.argmax(pred)), int(np.sum(test_example))))
+    else:
+        break
+
+
